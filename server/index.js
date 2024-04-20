13a698b2aa3381fa3ea18150e043de8696a59a81
Robertx999 <robert.e4200@gmail.com>
2024-04-20 15:16:53 +0200

    started migration from php to express

--- server/index.js
+++ server/index.js
@@ -1,5 +1,5 @@
-import express, { raw } from 'express';
-import { createServer } from 'http';
+import express from 'express';
+import { createServer } from 'node:http';
 import { WebSocketServer } from 'ws';
 import fs from 'node:fs';
 import { handler } from '../build/handler.js';
@@ -12,33 +12,24 @@ const server = createServer(app);
 const wsServer = new WebSocketServer({ port: ws_port });
 
 wsServer.on('connection', (socket, req) => {
-	let command = 'mac';
-	socket.send(command);
+	let command,
+		mac = undefined;
 	socket.on('message', (message) => {
 		// socket.send(message);
 		if (command == 'mac') {
 			console.log(req.socket.remoteAddress);
 			console.log(message.toString());
+			mac = message.toString();
 		}
-		// console.log(message.toString());
-		// fs.writeFile(
-		// 	'./server/uploads/image.jpg',
-		// 	message.toString(),
-		// 	{ encoding: 'base64' },
-		// 	function (err) {
-		// 		console.log('File created');
-		// 	}
-		// );
-		if (command == 'pic') {
-			fs.writeFile('./server/uploads/test.jpg', message, 'binary', function (err) {
+		if (command == 'pic' && mac != undefined) {
+			fs.writeFile(`./server/uploads/${Date.now()}-${mac}`, message, 'binary', function (err) {
 				if (err) throw err;
 				console.log('File saved.');
 			});
 		}
-		// fs.writeFile('./server/uploads/test.txt', message.toString(), {}, function (err) {
-		// 	console.log('File created');
-		// });
 	});
+	command = 'mac';
+	socket.send(command);
 	command = 'pic';
 	socket.send(command);
 	setInterval(() => {
@@ -51,6 +42,26 @@ wsServer.on('connection', (socket, req) => {
 // "data:image/jpeg;base64,"
 // SvelteKit should handle everything else using Express middleware
 // https://github.com/sveltejs/kit/tree/master/packages/adapter-node#custom-server
+app.get('/getimglist', (req, res) => {
+	let filteredFiles = [],
+		cams = new Map();
+	fs.readdir('./server/uploads/', (err, files) => {
+		if (err) throw err;
+		// files.filter((value) => value.)
+		filteredFiles = files.filter((value) =>
+			/^([0-9]*)([-]{1})([0-9A-Fa-f]{2}[:]){5}([0-9A-Fa-f]{2})$/.test(value)
+		);
+		filteredFiles.sort((a, b) => parseInt(a.split('-')[0]) - parseInt(b.split('-')[0]));
+		filteredFiles.forEach((value) => {
+			let mac = value.split('-')[1];
+			cams.set(mac, [...cams.get(mac), value]);
+		});
+	});
+	res.json({
+		paths: cams
+	});
+});
+
 app.use(handler);
 
 server.listen(port, '192.168.50.41');
